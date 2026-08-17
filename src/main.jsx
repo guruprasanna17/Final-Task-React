14b05eb3a1874c6b219411366c592b7dbabda7af
guruprasanna <guruprasannasubramani2001@gmail.com>
2026-08-17 19:51:49 +0530

    Fix Context folder case

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 
 import App from "./App";
-import { StudentProvider } from "./context/StudentContext";
+import { StudentProvider } from "./Context/StudentContext";
 
 import "./index.css";
 
